8b96101b99329183d48dc384d88c9aad0b4bff91
Prabhat Malhan <prabhatmalhan.cse@geu.ac.in>
2020-11-10 18:21:44 +0530

    eqality of sum of right and left subarray

---
+++ subarray_Sum.cpp
@@ -0,0 +1,37 @@
+/* WAP to input an array and find the element whose left and right subarray sum is equal*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>0)
+    {
+        int arr[n],sr,sl;
+        sr = sl = 0;
+        for(int i=0 ; i<n ; ++i)
+        {
+            cin>>arr[i];
+            sr += arr[i];
+        }
+        sl += arr[0];
+        sr -= arr[0];
+        for(int i=1 ; i<n-1 ; ++i)
+        {
+            sr -= arr[i];
+            if(sl == sr)
+            {
+                cout<<"Index : "<<i<<endl;
+                cout<<"Position : "<<i+1<<endl;
+                cout<<"Value : "<<arr[i]<<endl;
+                return 0;
+            }
+            sl += arr[i];
+        }
+        cout<<"No such index found"<<endl;
+    }
+    return 0;
+}
